6a1159f34fbf18581ab1556f7a255356eecee2a5
Pradeep Sharma <Pradeep.Sharma@BDINDIA.LOCAL>
2019-05-09 11:32:03 +0530

    assignment 2

--- ml/ex2/costFunction.m
+++ ml/ex2/costFunction.m
@@ -19,7 +19,10 @@ grad = zeros(size(theta));
 %
 % Note: grad should have the same dimensions as theta
 %
-
+h=sigmoid(X*theta);
+probOneTerm=(y*log(h));
+probZeroTerm=((1-y))*log(1-h);
+J = -(1/m)*(probOneTerm+probZeroTerm);
 
 
 
