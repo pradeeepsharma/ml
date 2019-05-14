3e100f18f8b70391a2f6dbca2e800b304b2b7a23
pradeeepsharma <pradeeepsharma@gmail.com>
2019-05-14 19:41:25 +0530

    week 2 assignment

--- ml/ex2/costFunctionReg.m
+++ ml/ex2/costFunctionReg.m
@@ -9,7 +9,7 @@ m = length(y); % number of training examples
 
 % You need to return the following variables correctly 
 J = 0;
-grad = zeros(size(theta));
+gradient = zeros(size(theta));
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta.
@@ -17,7 +17,22 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+h=sigmoid(X*theta);
 
+costJ = (-1/m) * sum( y .* log(h) + (1 - y) .* log(1 - h) );
+costRegularizationTerm = lambda/(2*m) * sum( theta(2:end).^2 );
+J = costJ + costRegularizationTerm;
+
+for i = 1:m
+		gradient = gradient + ( h(i) - y(i) ) * X(i, :)';
+end
+regularizationTerm = lambda/m * [0; theta(2:end)];
+grad = (1/m) * gradient + regularizationTerm; 
+
+%probOneTerm=((y')*log(h));
+%probZeroTerm=((1-y)')*log(1-h);
+%J = ((1/m)*(-probOneTerm-probZeroTerm))+(lambda/2*m)*theta(2:end).^2;
+%grad = (((1/m)*X')*((sigmoid(X*theta))-y))+[0; theta(2:end)];%(lambda/m)*theta;
 
 
 
