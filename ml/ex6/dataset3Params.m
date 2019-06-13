bbcfea13e810b474ae0e557a3ef25a543fc74419
pradeeepsharma <pradeeepsharma@gmail.com>
2019-06-13 21:52:31 +0530

    ex6 progress

--- ml/ex6/dataset3Params.m
+++ ml/ex6/dataset3Params.m
@@ -22,10 +22,17 @@ sigma = 0.3;
 %  Note: You can compute the prediction error using 
 %        mean(double(predictions ~= yval))
 %
-model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma)); 
-predictions = svmPredict(model, Xval);
-error = mean(double(predictions ~= yval));
-
+error = zeros(8, 8);
+c_test = [0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30];
+sigma_test = [0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30];
+for i=1:size(c_test,1)
+  for j=1:size(sigma_test,1)
+    model= svmTrain(X, y, c_test(i), @(x1, x2) gaussianKernel(x1, x2, sigma_test(j))); 
+    predictions = svmPredict(model, Xval);
+    error(i,j) = mean(double(predictions ~= yval));
+    
+  endfor
+endfor
 
 
 
